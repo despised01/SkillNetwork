509ee220507b37d0ba5e8e34ab7d5612fbfcf3b2
Антон Левин <despised01@gmail.com>
2023-07-02 11:17:46 +0300

    Добавлен 'UnitOfWork', 'AccountManagerController', 'LogingViewModel', 'Months'

---
+++ SkillNetwork/Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using SkillNetwork.Data.Repository;
+using System;
+
+namespace SkillNetwork.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges(bool ensureAutoHistory = false);
+
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class;
+    }
+}
